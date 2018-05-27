69d0a1059ec9d50a78ac15e11a7ea5343bd70ac2
Douglas Wagner <doug.gwagner@gmail.com>
2018-05-27 14:20:35 -0400

    Project Start
    Beginning of storyboard and main view controller

---
+++ MyWeatherApp/City.swift
@@ -0,0 +1,9 @@
+//
+//  City.swift
+//  MyWeatherApp
+//
+//  Created by Doug Wagner on 5/27/18.
+//  Copyright © 2018 Doug Wagner. All rights reserved.
+//
+
+import Foundation
---
+++ MyWeatherApp/CityViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MyWeatherApp
+//
+//  Created by Doug Wagner on 5/27/18.
+//  Copyright © 2018 Doug Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var HomeTableView: UITableView!
+    @IBOutlet weak var NewLocationButton: UIButton!
+    
+    var citiesArr = [City]()
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citiesArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        let city = citiesArr[indexPath.row]
+        
+        let label = cell.viewWithTag(110) as! UILabel
+        label.text = city.name
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
